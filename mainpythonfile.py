dad08ec942651f832177100b9ece4009af7b537a
James <james@zingel.co.nz>
2017-03-04 09:32:01 +1300

    Main Python File

---
+++ mainpythonfile.py
@@ -0,0 +1,13 @@
+#! python2
+
+#This is the main python file.
+#Please do not touch unless your are ABSOLUTELY SURE ABOUT WHAT YOU ARE DOING
+#Thanks :)
+
+import flask
+
+x = 10
+
+x += 1
+
+print(x)
