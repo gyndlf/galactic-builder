e91454f7d25ef2372039b17830411430158f06a9
James <james@zingel.co.nz>
2017-03-05 20:32:52 +1300

    Create user code

--- createUser.py
+++ createUser.py
@@ -1,4 +1,5 @@
 import pickle
+import os, sys
 
 # Creates a new user with no money, jobs or anything according to the database.py file
 
@@ -11,5 +12,16 @@ created = database.person
 
 created.name = name
 created.id = user
+
+MY_DIR  = os.path.realpath(os.path.dirname(__file__))
+PICKLE_DIR = os.path.join(MY_DIR, 'data')
+
+filename = str(created.name) + ".p"
+
+fname = os.path.join(PICKLE_DIR, filename)
+print("Creating " + str(fname))
+with open(fname, 'wb') as f:
+    pickle.dump(created, f)
+print("Created")
 
 
