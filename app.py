54c40ef8ea4edf4d31ef82709d1e12ad613b19a2
James <james@zingel.co.nz>
2017-03-09 19:06:48 +1300

    Tried to get buttons working

--- app.py
+++ app.py
@@ -43,12 +43,20 @@ def show_post(user):
     # show the post with the given id, the id is a string
     return "User = " + str(user)
 
+@app.route('/foo', methods=['POST'])
+def foo():
+    if request.method == 'POST':
+        x = 10
+    return render_template('teststuff.html')
+
+
 @app.route('/user/')
 @app.route('/user/<name>')
 def user(name=None):
     for person in users:
         if name == person.name:
-            return render_template('finances.html', money=person.money, netIncome=person.netIncome, income=person.income, expenses=person.expenses, netWorth=person.netWorth)
+            return render_template('finances.html', money=person.money, netIncome=person.netIncome, income=person.income,
+                                   expenses=person.expenses, netWorth=person.netWorth)
     return "Invaild username"
 
 if __name__ == "__main__":
