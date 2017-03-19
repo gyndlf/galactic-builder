3ce134f0480ea9f1a98273293ed7d77dfeaaf31b
James <james@zingel.co.nz>
2017-03-19 15:05:25 +1300

    Copied stuff from spreadsheet

--- functions/factories.py
+++ functions/factories.py
@@ -1,5 +1,17 @@
 #Has all factory functions
 
+#From the spreadsheet:
+#Income = Product cost * amount produced
+#Product cost = Constant value
+#Amount produced = 1.5^(Factory Level)*5
+#Factory Level = Upgradable Value
+#Profit = (Income) - (Material Cost * Amount Produced) - (Amount of workers * Worker Salary) {If less than $0, profit is $0}
+#Factory Upgrade Cost = Money Made * 3
+#Material Cost = Each resource * Resource cost
+#Amount of workers = Factory level * 10 (Change to 30?)
+#Salary = Constant Value
+
+
 class factory ():
     def __init__(self):
         pass
--- functions/mines.py
+++ functions/mines.py
@@ -1,5 +1,13 @@
 #Has all mine functions
 
+#From the spreadsheet
+#Amount mined = Mine Level * 100
+#Mine Level = Upgradable Value
+#Income = Amount mined * Mine Type Value
+#Mine Type Value = Profit per mine
+#Worker Count = Mine Level * 30
+#Profit = Income - (Worker count * 1000)
+
 class mine ():
     def __init__(self):
         pass
