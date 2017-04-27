4e1c4c5e4abe5367421993eaec89ea8b5dbd85bb
James <james@zingel.co.nz>
2017-04-27 16:23:37 +1200

    Boosted appllyinix value

--- baseValues.py
+++ baseValues.py
@@ -23,7 +23,7 @@ class basic ():
         self.factoryValues = {
             'Car' : 30000, #10 steel, 1 silicon,
             'xPhone' : 1000, #2 titianium, 1 copper, 2 silicon (Possible weapon license)
-            'AppyLyinx' : 2000, #5 titianium, 3 copper, 3 silicon, 1 noobidium
+            'AppyLyinx' : 10000, #5 titianium, 3 copper, 3 silicon, 1 noobidium
             'Toilet' : 50, #10 noobidium
             'Pii Fighter' : 100000, #50 steel, 20 titianium, 10 hydrogen, 1 white crystal, weapon lisence core, fighter license
             '4d Printer' : 200000 #10 steel, 10 copper, 10 silicon, 1 white crystal, 1 yellow crystal
