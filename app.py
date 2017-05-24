1c2de10e4360f318b1022dee825cb4d8963abe1c
James <james@zingel.co.nz>
2017-05-24 19:43:31 +1200

    Fixed links and touched up stuff

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler = logging.FileHandler('logs.log')
 handler.setFormatter(formatter)
 
- #Add quotes here for normal output
+''' #Add quotes here for normal output
 logger = logging.getLogger('')
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
@@ -28,7 +28,7 @@ logger.setLevel(logging.INFO)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-'''#Add quotes here for only printing to the log
+#Add quotes here for only printing to the log
 
 seed = 759478698707086794535275
 random.seed(seed)
@@ -654,5 +654,5 @@ def userButton(name=None):
 if __name__ == "__main__":
     # app.run(debug=True)
     logger.info('Running app')
-    #app.run()
-    app.run('0.0.0.0', 80)
+    app.run()
+    #app.run('0.0.0.0', 80)
