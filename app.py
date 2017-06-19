1d13e3319b1de95be165a5feb88c903bdf00008a
James <james@zingel.co.nz>
2017-06-19 18:56:57 +1200

    Release Alpha v5.1

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler = logging.FileHandler('logs.log')
 handler.setFormatter(formatter)
 
-'''#Add quotes here for normal output
+#Add quotes here for normal output
 logger = logging.getLogger('')
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
@@ -28,9 +28,9 @@ logger.setLevel(logging.INFO)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-#Add quotes here for only printing to the log
+'''#Add quotes here for only printing to the log
 
-seed = 759478698707086794535275
+seed = 19285718923740892364809237480923645
 random.seed(seed)
 logger.info('The seed is %s', seed)
 app = Flask(__name__)
@@ -679,5 +679,5 @@ def userButton(name=None):
 if __name__ == "__main__":
     # app.run(debug=True)
     logger.info('Running app')
-    app.run()
-    #app.run('0.0.0.0', 80)
+    #app.run()
+    app.run('0.0.0.0', 80)
