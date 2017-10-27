c4b0d07c3f289e565e06ead23b2a2a14d5e1ff22
James <james@zingel.co.nz>
2017-10-28 11:48:41 +1300

    Tsting

--- update_servers.sh
+++ update_servers.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
 echo "Updating Remotes..."
-git remote
-git pull origin # Pull origin to stay up to date
+#git remote
+#git pull origin # Pull origin to stay up to date
+
+PHR="Release Alpha v"
+NUM1=5 # Set new main version number here
+NUM2=5 # And set version minor number here
+
+TMP=$[NUM2-1]
+
+
+echo ""
+echo "Changing to"
+echo $NUM1.$NUM2
+echo "from"
+echo $NUM1.$TMP
+echo ""
+
+VER="$NUM1.$NUM2"
+MSG="$PHR$VER"
+
+#sed -e "206s/$NUM1.$TMP/$NUM1.$NUM2/" templates/index.html > templates/index_tmp.html # Update the new version num
+#mv templates/index_tmp.html templates/index.html
+
+git commit --all --message $MSG # Commit all the changed code
+
+git push origin # Update bitbucket
+git push heroku # Update heroku
+git push git # Update git
+#git push AAmazon # Update amazon server (legacy)
+#git push AHeroku # Update amazon / heroku sever  (legacy)
