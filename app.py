2aeccba17be0f40d92bc18297dbf27a793655d18
James <james@zingel.co.nz>
2017-11-08 16:09:09 +1300

    Tidying up app.py code

--- app.py
+++ app.py
@@ -6,7 +6,6 @@
 # export FLASK_APP=mainpythonfile.py
 # python -m flask run
 
-#Nope.
 
 import pickle
 import os
@@ -28,7 +27,7 @@ logger.setLevel(logging.INFO)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-#Add quotes here for only printing to the log
+# Add quotes here for only printing to the log
 
 seed = 19285718923740892364809237480923645
 random.seed(seed)
@@ -62,23 +61,23 @@ for i, c in enumerate(CHAR_SET):
 MY_DIR = os.path.realpath(os.path.dirname(__file__))
 PICKLE_DIR = os.path.join(MY_DIR, 'data')
 USERS = "users.p"
-USERSPATH = os.path.join(PICKLE_DIR, USERS)
+USERS_PATH = os.path.join(PICKLE_DIR, USERS)
 DATABASE = 'database.p'
-DATABASEPATH = os.path.join(PICKLE_DIR, DATABASE)
+DATABASE_PATH = os.path.join(PICKLE_DIR, DATABASE)
 VALUES = 'values.p'
-VALUESPATH = os.path.join(PICKLE_DIR, VALUES)
+VALUES_PATH = os.path.join(PICKLE_DIR, VALUES)
 
 
-def loadUsers():
-    '''Load users from the file given as USERS'''
+def loadusers():
+    """Load users from the file given as USERS"""
     logger.info('[1] Loading users')
     users = []
     # Load users.p files
-    with open(USERSPATH, 'rb') as f:
-        usersArray = pickle.load(f)
+    with open(USERS_PATH, 'rb') as f:
+        usersarray = pickle.load(f)
 
     # Read to find out the users
-    for file in usersArray:
+    for file in usersarray:
         fname = os.path.join(PICKLE_DIR, file)
         with open(fname, 'rb') as f:
             person = pickle.load(f)
@@ -86,16 +85,16 @@ def loadUsers():
     return users
 
 
-def loadValues():
-    '''Load the values from the file given as VALUES'''
+def loadvalues():
+    """Load the values from the file given as VALUES"""
     logger.info('[1] Load values')
-    with open(VALUESPATH, 'rb') as f:
+    with open(VALUES_PATH, 'rb') as f:
         values = pickle.load(f)
     return values
 
 
 def scrambleCookie(request, username):
-    '''Scramble the username and add to the give request'''
+    """Scramble the username and add to the give request"""
     t = []
     for i in username:
         v = ENCRYPT_DICT[i]
@@ -106,7 +105,7 @@ def scrambleCookie(request, username):
 
 
 def loadCookie(username):
-    '''Decrypt the username'''
+    """Decrypt the username"""
     # Converts the cypher to words.
     h = []
     for i in username:
@@ -116,7 +115,7 @@ def loadCookie(username):
 
 
 def hasMoney(object, money):
-    '''Weather or not the object has >= money'''
+    """Weather or not the object has >= money"""
     try:
         if object.money >= money:
             return True
@@ -127,7 +126,7 @@ def hasMoney(object, money):
 
 
 def total(users, values):
-    '''Calculate totals across all the users'''
+    """Calculate totals across all the users"""
     logger.info('[2] Running total definition')
 
     foodSent = 0
@@ -143,7 +142,7 @@ def total(users, values):
     for person in users:
         foodSent += person.foodProduced
         totalMoney += person.money
-        tmpWealth = int(person.netIncome * random.randint(7500,12500)/10000)
+        tmpWealth = int(person.netIncome * random.randint(7500, 12500) / 10000)
         if tmpWealth < 1:
             tmpWealth = 0
         wealth[person.name] = tmpWealth
@@ -158,7 +157,7 @@ def total(users, values):
     for item in wealth:
         avgWealth += wealth[item]
         count += 1
-    avgWealth = int(round((avgWealth/count),0))
+    avgWealth = int(round((avgWealth / count), 0))
 
     # Put them in a dictionary
     calculated = {
@@ -167,14 +166,14 @@ def total(users, values):
         'totalMines': totalMines,
         'factoryCount': factories,
         'wealth': wealth,
-        'avgWealth' : avgWealth
+        'avgWealth': avgWealth
     }
     logger.debug('Calculated factory count %s', factories)
     return calculated
 
 
 def farm(values, totals):
-    '''The farms calculation. Calculates all the formulas for farms'''
+    """The farms calculation. Calculates all the formulas for farms"""
     logger.info('[3] Running farms def')
     # All farm calculations done here. Not farms.py
 
@@ -196,7 +195,7 @@ def farm(values, totals):
 
 
 def mine(values, totals):
-    '''Calculate the mine costs'''
+    """Calculate the mine costs"""
     logger.info('[3] Running mine def')
     calculated = {}
     # For each mine calculate its costs
@@ -209,8 +208,8 @@ def mine(values, totals):
         logger.debug(msg)
         calculated[mine] = mineCost
 
-    #Mine upgrades
-    base = int(totals['avgWealth']/10)
+    # Mine upgrades
+    base = int(totals['avgWealth'] / 10)
     if base < 100:
         base = 100
     root10 = base * 10
@@ -218,18 +217,18 @@ def mine(values, totals):
     root100 = base * 100
     root2 = root100 * root50 * root10 * base
     mineUpgrades = {
-        1 : base,
-        10 : root10,
-        50 : root50,
-        100 : root100,
-        2 : root2
+        1: base,
+        10: root10,
+        50: root50,
+        100: root100,
+        2: root2
     }
     calculated['mineUpgrades'] = mineUpgrades
     return calculated
 
 
 def factory(values, totals):
-    '''Calculate factory costs'''
+    """Calculate factory costs"""
 
     ''' respourceDisplay = 
     for mine in values.mineValues:
@@ -243,16 +242,16 @@ def factory(values, totals):
 
     calculated = {}
 
-    #Calculate factory costs
+    # Calculate factory costs
     for factoryt in values.factoryValues:
 
-        #Make sure that the scaling is correct. Now timesing by 0
+        # Make sure that the scaling is correct. Now timesing by 0
         if totals['factoryCount'][factoryt] == 0:
             calculated[factoryt] = values.factoryValues[factoryt] * 2
         else:
             calculated[factoryt] = values.factoryValues[factoryt] * totals['factoryCount'][factoryt] * 2
 
-        #Make sure the cost is not below 10
+        # Make sure the cost is not below 10
         if calculated[factoryt] < 10:
             calculated[factoryt] = 10
     logger.debug('Calculated factory cost %s', calculated)
@@ -267,7 +266,7 @@ def factory(values, totals):
 
 
 def dynamicPersonalCalc(object, farms, values):
-    '''Calculate personal dynamic varibles'''
+    """Calculate personal dynamic varibles"""
     logger.info('[4] Running dynamic personal calulator def')
 
     # Farms
@@ -277,11 +276,11 @@ def dynamicPersonalCalc(object, farms, values):
     # Mines
     minesDict = {}
     # amount produced = number of mines * percentage boost || Eventually use this
-    #object.mineBoost is representing a number/100. So 5 is a 5% bonus.
-    #object.minePowerUpgrade is the one that lets mines produce more than one material
+    # object.mineBoost is representing a number/100. So 5 is a 5% bonus.
+    # object.minePowerUpgrade is the one that lets mines produce more than one material
     for name in values.mineValues:
         produced = object.ownedMines[name] * object.minePowerUpgrade
-        produced += int(produced * object.mineBoost/100)
+        produced += int(produced * object.mineBoost / 100)
         minesDict[name] = produced
 
     # Factories
@@ -299,9 +298,9 @@ def dynamicPersonalCalc(object, farms, values):
         facProfit = facIncome - 0  # Need to add materials next
 
         localMaterialsNeeded = {}
-        for material in values.factoryRecipies[factory]: #Find out how many materials are needed
+        for material in values.factoryRecipies[factory]:  # Find out how many materials are needed
             materialsNeeded[material] += values.factoryRecipies[factory][material] * facProduced
-            #Put a minimum value in case they produce nothing in that factory
+            # Put a minimum value in case they produce nothing in that factory
             toSet = values.factoryRecipies[factory][material] * facProduced
             if toSet > 0:
                 localMaterialsNeeded[material] = toSet
@@ -313,26 +312,25 @@ def dynamicPersonalCalc(object, farms, values):
             'produced': facProduced,
             'income': facIncome,
             'profit': facProfit,
-            'needed' : localMaterialsNeeded,
-            'materialsMade' : '(Included in a future update)'
+            'needed': localMaterialsNeeded,
+            'materialsMade': '(Included in a future update)'
         }
 
         factoryDict[factory] = tmp
     logger.debug('Materials needed in total %s', materialsNeeded)
     logger.debug('Materials produced: %s', minesDict)
 
-    #Effectivly materialsNeeded - materialsProduced
-    materialsToBuy = { k: materialsNeeded.get(k, 0) - minesDict.get(k, 0) for k in set(materialsNeeded) & set(minesDict) }
-    for material in materialsToBuy: #Make sure they are not negative numbers!
+    # Effectivly materialsNeeded - materialsProduced
+    materialsToBuy = {k: materialsNeeded.get(k, 0) - minesDict.get(k, 0) for k in set(materialsNeeded) & set(minesDict)}
+    for material in materialsToBuy:  # Make sure they are not negative numbers!
         if materialsToBuy[material] < 0:
             materialsToBuy[material] = 0
     logger.debug('Materials to buy: %s', materialsToBuy)
 
-    materialCost = 0 #Add up all the costs
+    materialCost = 0  # Add up all the costs
     for material in materialsToBuy:
         materialCost += materialsToBuy[material] * values.mineValues[material]
     logger.debug('You have a factory material cost of %s', materialCost)
-
 
     # General
     income = Fincome + totalFacIncome  # Add factory income and mine income here
@@ -370,15 +368,16 @@ def dynamicPersonalCalc(object, farms, values):
 # All app.route functions -----------------------------------------------------------------------------------------#
 @app.route('/')
 def home():
-    '''The main login page / Index'''
-    #return redirect(url_for('user', name='james', page='home'))  # A little hotwire for debuging. Remember to remove cookie stuff
+    """The main login page / Index"""
+    # return redirect(url_for('user', name='james', page='home'))
+    # A little hotwire for debuging. Remember to remove cookie stuff
     return render_template('index.html')
 
 
 @app.route('/loginuser', methods=['POST'])
 def calcmessage():
-    '''This is the login script'''
-    users = loadUsers()
+    """This is the login script"""
+    users = loadusers()
     try:
         username = request.form['username']
         password = request.form['password']
@@ -402,10 +401,10 @@ def calcmessage():
 @app.route('/user/<name>/<page>')
 @app.route('/user/<name>/<page>/<data>')
 def user(name=None, page=None, data=None):
-    '''The main script. Run whenever logged in'''
+    """The main script. Run whenever logged in"""
     logger.info("-" * 10 + str("Finances") + "-" * 10)
 
-    #Make sure the user is allowed to be here!
+    # Make sure the user is allowed to be here!
     try:  # Load the session cookie
         cookie = request.cookies.get('sessionID')
         logger.debug('Username via cookie: %s', cookie)
@@ -413,13 +412,13 @@ def user(name=None, page=None, data=None):
         logger.error('No cookie found')
         return redirect(url_for('home'))
 
-    #Check the cookie matches
+    # Check the cookie matches
     cookie = loadCookie(cookie)
     if cookie != name:
         logger.error('Cookie is not the same as %s', name)
         return 'You do not have access to this location'
 
-    #Have you been sent here with an error? Get ready to display it!
+    # Have you been sent here with an error? Get ready to display it!
     if data == 'notEnoughMoney':
         dialogMessage = 'Not enough money!'
     elif data == 'maxFarmLevel':
@@ -428,8 +427,8 @@ def user(name=None, page=None, data=None):
         dialogMessage = None
 
     # Calculate the very basics
-    values = loadValues()
-    users = loadUsers()
+    values = loadvalues()
+    users = loadusers()
     totals = total(users, values)
 
     # Calculate the recipies
@@ -471,7 +470,8 @@ def user(name=None, page=None, data=None):
                     'person': person
                 }
                 return render_template('mines.html', username=person.name, money=person.money,
-                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name], dialogMessage=dialogMessage, **templateData)
+                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name],
+                                       dialogMessage=dialogMessage, **templateData)
 
             elif page == 'factories':
                 logger.info("Rendering factories html...")
@@ -482,7 +482,8 @@ def user(name=None, page=None, data=None):
                     'totals': totals
                 }
                 return render_template('factories.html', username=person.name, money=person.money,
-                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name], dialogMessage=dialogMessage, **templateData)
+                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name],
+                                       dialogMessage=dialogMessage, **templateData)
 
             elif page == 'community':
                 logger.info("Rendering community html..")
@@ -494,27 +495,32 @@ def user(name=None, page=None, data=None):
                     labels.append(item)
                 colors = ["#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA", "#ABCDEF", "#DDDDDD"]
                 return render_template('community.html', username=person.name, money=person.money,
-                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name], dialogMessage=dialogMessage, set=zip(info, labels, colors))
+                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name],
+                                       dialogMessage=dialogMessage, set=zip(info, labels, colors))
 
             elif page == 'species':
                 logger.info('Rendering species html')
                 return render_template('species.html', username=person.name, money=person.money,
-                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name], dialogMessage=dialogMessage)
+                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name],
+                                       dialogMessage=dialogMessage)
 
             elif page == 'war':
                 logger.info('Rendering war html')
                 return render_template('war.html', username=person.name, money=person.money,
-                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name], dialogMessage=dialogMessage)
-                
+                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name],
+                                       dialogMessage=dialogMessage)
+
             elif page == 'shipBuying':
                 logger.info('Rendering shipBuying html')
                 return render_template('shipBuying.html', username=person.name, money=person.money,
-                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name], dialogMessage=dialogMessage, values=values)
+                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name],
+                                       dialogMessage=dialogMessage, values=values)
 
             elif page == 'diplomacy':
                 logger.info('Rendering shipBuying html')
                 return render_template('diplomacy.html', username=person.name, money=person.money,
-                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name], dialogMessage=dialogMessage)
+                                       netIncome=person.netIncome, netWorth=totals['wealth'][person.name],
+                                       dialogMessage=dialogMessage)
 
             else:
                 logger.error('Invalid page name!')
@@ -525,7 +531,7 @@ def user(name=None, page=None, data=None):
 
 @app.route('/user/<name>/button', methods=['POST'])
 def userButton(name=None):
-    '''The script runs once ANY button is pressed'''
+    """The script runs once ANY button is pressed"""
     logger.info("-" * 10 + str("Button") + "-" * 10)
 
     try:  # Load the session cookie
@@ -542,20 +548,20 @@ def userButton(name=None):
 
     # Run recipies
     logger.info("Loading recipies...")
-    users = loadUsers()
-    values = loadValues()
+    users = loadusers()
+    values = loadvalues()
     totals = total(users, values)
     farms = farm(values, totals)
     mines = mine(values, totals)
     factories = factory(values, totals)
 
-    #Where have you come from?
+    # Where have you come from?
     farmHtml = False
     mineHtml = False
     factoryHtml = False
     shipBuyHtml = False
 
-    #Has there been an error you should know about?
+    # Has there been an error you should know about?
     error = None
 
     # Identify the user
@@ -604,7 +610,7 @@ def userButton(name=None):
                     error = 'notEnoughMoney'
 
             else:
-                mineUpgrades = [1,10,50,100]
+                mineUpgrades = [1, 10, 50, 100]
                 for percent in mineUpgrades:
                     title = 'mineUpgrade' + str(percent)
                     if title in request.form:
@@ -644,7 +650,7 @@ def userButton(name=None):
                         else:
                             logger.error("Money Error: Not enough money")
                             error = 'notEnoughMoney'
-                            
+
                 for ship in person.ownedShips:
                     button = 'buy' + ship
                     if button in request.form:
@@ -697,5 +703,4 @@ if __name__ == "__main__":
     # app.run(debug=True)
     logger.info('Running app')
     app.run()
-    #app.run('0.0.0.0', 80)
-
+    # app.run('0.0.0.0', 80)
